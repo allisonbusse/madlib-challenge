cee9b12c904c070e7a710451c0202c9c8a426c4a
allisonbusse <allisonbusse@gmail.com>
2019-07-10 20:54:05 -0700

    added madlib app

---
+++ app.js
@@ -0,0 +1,22 @@
+// Reference needed elements
+const adjective = document.getElementById('adjective-1');
+const adjectiv2e = document.getElementById('adjective-2');
+const number = document.getElementById('number');
+const verb = document.getElementById('verb-1');
+const noun = document.getElementById('noun');
+const adjectiveResult = document.getElementById('adjective-result-1');
+const adjectiv2eResult = document.getElementById('adjective-result-2');
+const numberResult = document.getElementById('number-result');
+const verbResult = document.getElementById('verb-result-1');
+const nounResult = document.getElementById('noun-result');
+
+
+// Actions to get elements
+// eslint-disable-next-line no-unused-vars
+function doThing() {
+    adjectiveResult.textContent = adjective.value;
+    adjectiv2eResult.textContent = adjectiv2e.value;
+    numberResult.textContent = number.value;
+    verbResult.textContent = verb.value;
+    nounResult.textContent = noun.value;
+} 
